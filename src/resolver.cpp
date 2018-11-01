e7bcde7da2ae9490ae7232331b117f940e408b56
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2018-11-01 11:54:48 +0100

    non working coro version
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -1,36 +1,53 @@
 #include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include <functional>
 #include <string_view>
 #include <iostream>
 
 using tcp = boost::asio::ip::tcp;
 
-void resolve_request(boost::asio::io_context& ioc,
-		     std::string_view host,
-		     std::string_view port)
-{
-    tcp::resolver  asio_resolver {ioc};
-    auto const results = asio_resolver.resolve(host, port);
-    for (const auto& result: results){
+void print(const tcp::resolver::results_type& results){
+    for(const auto& result:results){
 	std::cout << result.endpoint() << std::endl;
     }
 }
 
-void async_resolve_request(boost::asio::io_context& ioc,
+void resolve_request(tcp::resolver& asio_resolver,
+		     std::string_view host,
+		     std::string_view port)
+{
+    auto const results = asio_resolver.resolve(host, port);
+    print(results);
+}
+
+void async_resolve_request(tcp::resolver& asio_resolver,
 			   std::string_view host,
 			   std::string_view port)
 {
-    tcp::resolver  asio_resolver {ioc};
     asio_resolver.async_resolve(host, port, []
 				(const boost::system::error_code& ec,
 				 tcp::resolver::results_type results){
 				    if (ec){
-					std::cout << "error " << ec.message() << std::endl;
+					std::cout << __func__ << " error " << ec.message() << std::endl;
 					return;
 				    }
-				    for  (const auto& result: results){
-					std::cout << result.endpoint() << std::endl;
-				    }
+				    print(results);
 				});
+}
+
+void coro_resolve_request(tcp::resolver& asio_resolver,
+			  std::string_view host,
+			  std::string_view port,
+			  boost::asio::yield_context yield_){
+    boost::system::error_code ec;
+    const auto results = asio_resolver.async_resolve(host, port, yield_[ec]);
+    if (ec){
+	std::cout << __func__ << " error " << ec.message() << std::endl;
+	return;
+    }
+    print(results);
+    
 }
 int main(int argc, char *argv[])
 {
@@ -43,8 +60,16 @@ int main(int argc, char *argv[])
 	port = argv[2];
 	
     boost::asio::io_context ioc;
-    resolve_request(ioc, argv[1], port);
-    async_resolve_request(ioc, argv[1], port);
+    tcp::resolver  asio_resolver {ioc};
+    resolve_request(asio_resolver, argv[1], port);
+    async_resolve_request(asio_resolver, argv[1], port);
+
+    // boost::asio::spawn(ioc, std::bind(
+    // 			   &coro_resolve_request,
+    // 			   asio_resolver,
+    // 			   argv[1],
+    // 			   port,
+    // 			   std::placeholders::_1));
     ioc.run();
     
 }
